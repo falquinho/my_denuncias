c71a0a84f3c2924d361aa2d679ad8b5db9c15b12
Lucas Bessa <lucas.falcao.nb@gmail.com>
2019-01-04 11:37:01 -0300

    Mecanismo de captura de foto.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Camera } from '@ionic-native/camera';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -28,7 +29,8 @@ import { NewDenunciaPageModule } from '../pages/new-denuncia/new-denuncia.module
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Camera
   ]
 })
 export class AppModule {}
--- src/pages/new-denuncia/new-denuncia.ts
+++ src/pages/new-denuncia/new-denuncia.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 /**
  * Generated class for the NewDenunciaPage page.
@@ -15,22 +16,49 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class NewDenunciaPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  base64img: string = undefined;
 
-
+  
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    private camera: Camera,
+    private toastCtrl: ToastController
+  ) {}
+    
+    
   ionViewDidLoad() {
     console.log('ionViewDidLoad NewDenunciaPage');
   }
-
-
+  
+  
   submitBtnClicked() {
     //
   }
-
+  
   
   cameraBtnClicked() {
-    //
+
+    const cam_config: CameraOptions = {
+      encodingType:    this.camera.EncodingType.JPEG,
+      mediaType:       this.camera.MediaType.PICTURE,
+      destinationType: this.camera.DestinationType.DATA_URL, // DATA_URL == foto codificada base64
+      targetHeight:    1280, // nao ha necessidade da imagem em resoluçao maxima 
+      targetWidth:     1280, // imagem 1:1 ja que vai ser contida em um circulo
+      quality:         90
+    }
+
+    this.camera.getPicture( cam_config ).then( data => {
+      // necessário adicionar cabeçalho a data para obter imagem base64 valida
+      this.base64img = 'data:image/jpeg;base64,' + data;
+
+    }, error => {
+      this.toastCtrl.create({
+        message: "Erro ao capturar imagem.",
+        duration: 3000
+      }).present();
+      
+    });
   }
 
 }
